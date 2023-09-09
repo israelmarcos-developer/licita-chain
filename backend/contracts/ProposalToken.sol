603eb07cacb0402778c8ca6eac08e384a02ff94b
João Vitor <jv.muca@gmail.com>
2023-09-08 21:05:23 -0300

    formating contracts

--- backend/contracts/ProposalToken.sol
+++ backend/contracts/ProposalToken.sol
@@ -4,24 +4,21 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ProposalToken is Ownable {
-
     event ValueChanged(address indexed whoChanged, uint256 indexed newValue);
 
     uint256 value;
     string info;
 
-    constructor(uint _value, string memory _info) 
-    Ownable()
-    {
+    constructor(uint _value, string memory _info) Ownable() {
         value = _value;
         info = _info;
     }
 
-    function getValue() external view returns(uint256) {
+    function getValue() external view returns (uint256) {
         return value;
     }
 
-    function getInfo() external view returns(string memory) {
+    function getInfo() external view returns (string memory) {
         return info;
     }
 
@@ -29,5 +26,4 @@ contract ProposalToken is Ownable {
         value = _newValue;
         emit ValueChanged(msg.sender, _newValue);
     }
-
 }
