44ff56eb79a171834a9fce2ddcc3760ebdc6db97
Israel Marcos <israelmarcos.developer@gmail.com>
2023-09-10 22:21:51 -0300

    leyout-fixes

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -40,7 +40,7 @@ const WalletButton = () => {
                 <p className='title-custon'>Identifique-se no LicitaChain</p>
                 <h1 className='title2-custon'> Licita-Chain</h1>
                 <div className='img-metamak-custon'>
-                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/1200px-MetaMask_Fox.svg.png" width="150" height="150" alt="Logo" />
+                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/1200px-MetaMask_Fox.svg.png" width="100" height="100" alt="Logo" />
                 </div>
                 <div className='button-custon'>
                     <Button
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,15 @@ export default function Home() {
   return (
     <main>
       <MenuNavigation />
-      <div className='container-login-custon'>
+      <div className='container custon-home'>
         <Row>
           <Col>
-            <Image src="https://sso.acesso.gov.br/assets/govbr/img/conta_govbr_v2.jpg" alt='img' fluid />
+            <Image src="https://sso.acesso.gov.br/assets/govbr/img/conta_govbr_v2.jpg" width="600" alt='img' fluid />
           </Col>
           <Col>
+          <div className='container'>
             <WalletButton />
+          </div>
           </Col>
         </Row>
       </div>
